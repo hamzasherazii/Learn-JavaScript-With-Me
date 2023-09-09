8932dfaedf935f2be26f9a189e34f9f6da8ce1d5
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-09-09 15:50:06 +0500

    styled css file with js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var link1 = document.getElementById("link1");
 console.log(link1.innerText);
 
-document.getElementById("link1").textContent = "Clcik this!";
+document.getElementById("link1").textContent = "Click this!";
 console.log(link1);
 
 // ------------
 
 document.getElementsByClassName("para1")[0].textContent = "first paragraph";
+
+var myBtn = document.getElementById("link2");
+myBtn.style.color = "green";
