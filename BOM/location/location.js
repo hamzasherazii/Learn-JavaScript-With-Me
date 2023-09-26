3d7576582ba9fa6eece3291f05b6b04d1b79ea74
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-09-26 17:26:54 +0500

    learned location in BOM

---
+++ BOM/location/location.js
@@ -0,0 +1,12 @@
+document.write(window.location + "<br>");
+document.write(window.location.href + "<br>");
+document.write(window.location.hostname + "<br>");
+document.write(window.location.pathname + "<br>");
+document.write(window.location.protocol);
+
+
+// var btnclk = window.location.assign("../window/window.html");
+
+function btnclk(){
+    window.location.assign("../window/window.html");
+}
