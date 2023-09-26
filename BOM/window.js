246aa7a812dc122b62c1efb6af1aef2ddaccd51a
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-09-26 17:03:36 +0500

    buttons in BOM

--- BOM/window.js
+++ BOM/window.js
@@ -1,2 +1,14 @@
 // The Browser Object Model (BOM) allows JavaScript to "talk" to the browser.
 document.write(window.innerHeight + " | " + window.innerWidth);
+
+// window.open();
+
+var meriWindow = window.open("", "hamza sherazi", "width=600,height=500");
+
+// Wait for the new window to load before adding the button.
+meriWindow.document.addEventListener("DOMContentLoaded", function() {
+    var winButton = meriWindow.document.createElement("button");
+    winButton.textContent = "Click me!";
+    meriWindow.document.body.appendChild(winButton);
+  });
+  
