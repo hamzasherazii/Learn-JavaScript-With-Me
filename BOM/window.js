b982976ed140b3a1582acb3d6189984668ed4fb0
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-09-26 17:17:55 +0500

    started learning screen

--- BOM/window.js
+++ BOM/window.js
@@ -1,14 +1,8 @@
 // The Browser Object Model (BOM) allows JavaScript to "talk" to the browser.
 document.write(window.innerHeight + " | " + window.innerWidth);
 
-// window.open();
+// window.open("https://www.google.com/");
 
-var meriWindow = window.open("", "hamza sherazi", "width=600,height=500");
-
-// Wait for the new window to load before adding the button.
-meriWindow.document.addEventListener("DOMContentLoaded", function() {
-    var winButton = meriWindow.document.createElement("button");
-    winButton.textContent = "Click me!";
-    meriWindow.document.body.appendChild(winButton);
-  });
-  
+function btnclk(){
+    window.close();
+}
