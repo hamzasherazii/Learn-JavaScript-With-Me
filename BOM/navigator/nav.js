e42cd5fae35b2718136886ca29d0b97c475bc1a5
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-09-26 18:28:54 +0500

    learned navigator

---
+++ BOM/navigator/nav.js
@@ -0,0 +1,8 @@
+document.write(navigator.cookieEnabled + "<br>");
+document.write(navigator.appCodeName + "<br>");
+document.write(navigator.appVersion + "<br>");
+document.write(navigator.platform + "<br>");
+document.write(navigator.product + "<br>");
+document.write(navigator.language + "<br>");
+document.write(navigator.userAgent + "<br>");
+document.write(navigator.javaEnabled + "<br>");
