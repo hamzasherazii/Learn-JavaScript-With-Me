d7dd91484d771f7dbb121a44898cb737dd185a2c
Hamza Sherazi <98487157+hamzasherazii@users.noreply.github.com>
2023-09-26 18:53:43 +0500

    learned functions

---
+++ functions/1.js
@@ -0,0 +1,13 @@
+// document.getElementById("heading1").style.color = "orange";
+// document.getElementById("heading2").style.color = "red";
+// document.getElementById("heading3").style.color = "blue";
+// document.getElementById("heading4").style.color = "green";
+
+
+function changeCSS() {
+    document.getElementById("heading1").style.color = "orange";
+    document.getElementById("heading2").style.color = "red";
+    document.getElementById("heading3").style.color = "blue";
+    document.getElementById("heading4").style.color = "green";
+}
+// changeCSS();
