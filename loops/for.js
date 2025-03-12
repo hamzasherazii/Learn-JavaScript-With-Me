097088340b4eb4a1f94a33d0c5f6bd0c8bf0980c
Hamza Sherazi <sherazihamza2k3@gmail.com>
2025-03-12 17:15:36 +0500

    loops

---
+++ loops/for.js
@@ -0,0 +1,4 @@
+// This is a simple for loop that counts from 1 to 5
+for (let i = 1; i <= 5; i++) {
+    console.log(i); // This will print the current value of i to the console
+}
---
+++ loops/while.js
@@ -0,0 +1,21 @@
+// This is a simple example of a while loop in JavaScript
+
+// Initialize a counter variable
+let counter = 0;
+
+// The while loop will run as long as the condition (counter < 5) is true
+while (counter < 5) {
+    // Print the current value of counter
+    console.log(counter);
+    
+    // Increment the counter by 1
+    counter++;
+}
+
+// Explanation:
+// 1. The counter variable is initialized to 0.
+// 2. The while loop checks if the counter is less than 5.
+// 3. If the condition is true, the code inside the loop is executed.
+// 4. The current value of counter is printed to the console.
+// 5. The counter is incremented by 1.
+// 6. The loop repeats until the counter is no longer less than 5.
