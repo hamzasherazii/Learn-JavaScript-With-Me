e8b19c63d59eb5a47bed62aeb4d730fa4136a74b
Hamza Sherazi <sherazihamza2k3@gmail.com>
2025-03-12 16:13:25 +0500

    data types

---
+++ data-types/all.js
@@ -0,0 +1,18 @@
+let name = "Hamza Sherazi";
+console.log(name); // Hamza Sherazi
+
+let age = 22;
+console.log(age); // 22 
+
+let isMarried = false;
+console.log(isMarried); // false
+
+let subjects = ["IOT", "AI", "CNC"];
+console.log(subjects); // {"IOT", "AI", "CNC"}
+
+let info = {
+    name: "Hamza Sherazi",
+    age: 22,
+    isMarried: false,
+}
+console.log(info); // {name: "Hamza Sherazi", age: 22, isMarried: false, subjects: {"IOT", "AI", "CNC"}}
