ddd8995783a6782687e09715901c9d5bae48b925
Hamza Sherazi <sherazihamza2k3@gmail.com>
2025-03-18 03:57:08 +0500

    practice

--- concepts/prac1.js
+++ concepts/prac1.js
@@ -1,15 +1,19 @@
+function swapStrings(str1, str2) {
+    console.log("Before Swap:");
+    console.log("str1:", str1);
+    console.log("str2:", str2);
+
+    // Swapping the strings
+    let temp = str1;
+    str1 = str2;
+    str2 = temp;
+
+    console.log("After Swap:");
+    console.log("str1:", str1);
+    console.log("str2:", str2);
+}
+
 let str1 = "Hello";
 let str2 = "World";
 
-console.log("Before Swap:");
-console.log("str1:", str1);
-console.log("str2:", str2);
-
-// Swapping the strings
-let temp = str1;
-str1 = str2; // str1 = "World"
-str2 = temp; // str2 = "Hello"
-
-console.log("After Swap:");
-console.log("str1:", str1);
-console.log("str2:", str2);
+swapStrings(str1, str2);
